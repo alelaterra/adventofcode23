fbdfd5081e9a3c176ca2575ea5f0336c231d7c60
alelaterra <57683440+alelaterra@users.noreply.github.com>
2023-12-13 23:02:59 +0000

    day4 part 1 completed

--- day4/index.js
+++ day4/index.js
@@ -3,10 +3,23 @@ function stringToMatrix(str) {
 }
 
 const matrix = stringToMatrix(input);
-
+let counter = 0;
 for(row in matrix){
-    const winningStr = matrix[row].split(':')[1].split('|')[0].trim();
-    const numbersStr = matrix[row].split(':')[1].split('|')[1].trim();
-    const card = { winning: winningStr, numbers: numbersStr}
-    console.log(card)
+    let rowCounter = 0;
+    const winning = matrix[row].split(':')[1].split('|')[0].trim().split(' ').map((w) => parseInt(w));
+    const numbers = matrix[row].split(':')[1].split('|')[1].trim().split(' ').map((w) => parseInt(w));
+    const card = { winning: winning, numbers: numbers}
+    let countWin = 0;
+    for(number of numbers){
+        if (winning.indexOf(number) !== -1) {
+            if(rowCounter === 0){
+                rowCounter = 1;
+            }else{
+                rowCounter *= 2
+            }
+        }
+    }
+    counter += rowCounter;
+    // console.log(card)
 }
+console.log(counter);
